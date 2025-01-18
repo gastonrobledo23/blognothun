174f554ce45705c542e42f5409e9085f3c4d3de3
gastonrobledo23 <gastonrobledo44@gmail.com>
2025-01-18 12:53:56 +0100

    1.2

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,18 @@
 
 export const SITE_TITLE = 'AstroG Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+//Crea un esquema de contenido para tus posts. En src/content/config.ts:
+import { defineCollection, z } from 'astro:content';
+
+const postsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    description: z.string(),
+    slug: z.string(),
+  }),
+});
+
+export const collections = {
+  posts: postsCollection,
+};
